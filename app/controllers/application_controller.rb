0450c8ef82ab96b2fd676671de32b887d9ba0104
Jay Parikh <jayparikh7@users.noreply.github.com>
2017-06-09 02:53:54 -0400

    Changes to Devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
+
+  before_action :authenticate_user!
+
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, :keys => [:username, :first_name, :last_name, :photo, :bio, :team_owner, :phone])
+
+    devise_parameter_sanitizer.permit(:account_update, :keys => [:photo, :bio, :team_owner, :phone])
+  end
 end
